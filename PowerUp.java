9a4017b519c7cebca4c33f98ed5eba4b644d2425
Khayr_Majid <co240145@adzu.edu.ph>
2025-12-02 23:32:06 +0800

    changes so far -fixed the bug where ball gets stuck in paddle -made it endless(resets when all blocks are destroyed) -initial implementation of buffs

---
+++ PowerUp.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+
+public class PowerUp {
+
+    public int x, y;
+    public int width = 20;
+    public int height = 20;
+
+    public int speed = 2;
+    public boolean active = true;
+
+    public int type; // 0 = bigger paddle, 1 = slower ball, etc.
+
+    public PowerUp(int x, int y, int type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        if (!active) return;
+
+        switch (type) {
+            case 0: g.setColor(Color.CYAN); break;       // bigger paddle
+            case 1: g.setColor(Color.MAGENTA); break;    // slower ball
+            case 2: g.setColor(Color.ORANGE); break;     // extra points  
+        }
+
+        g.fillOval(x, y, width, height);
+    }
+
+    public void update() {
+        y += speed;
+    }
+}
